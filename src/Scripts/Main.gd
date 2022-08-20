8b1dff1cf46c5afdbb2d6bac94d6e2229467f423
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-08-20 19:26:19 -0300

    feat: added event in adjacent buildings with its id

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -1,30 +1,29 @@
 extends Spatial
 var resources = {} 
 var input = {}
-export(int) var number_of_instances = 5
+export(int) var number_of_instances_main_building = 9
 export(int) var number_of_instances_adjacent_building = 4
+export(int) var gap = 10
 var resource_types = ["oil", "power", "water", "solar"]
 
-func _on_gui_Click(id,type):
+func _on_gui_Click(id):
 	print("id: " , id)
-	print("type: " , type)
-	input[type].append(id)
+#	input[type].append(id)
 	print(input)
 
-func generate_sequence(type):
-	for i in range(number_of_instances):
-		var resource = preload("res://Scenes/Resource.tscn").instance()
-		var initial_position = Vector3(randi() % 15, 0.0, randi() % 15)
-		resource.init(i,initial_position,type)
-		resource.connect("click",self,"_on_gui_Click")
-		add_child(resource)
-		resources[type].append(i)
-		for building in range(number_of_instances_adjacent_building):
-			var adjacent_position = adjacent_coordinates(initial_position, building)
-			var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-			adjacent_building.init(i,adjacent_position,type)
-			adjacent_building.connect("click",self,"_on_gui_Click")
-			add_child(adjacent_building)
+func main_building_coordidate(i, j):
+	var resource = preload("res://Scenes/Resource.tscn").instance()
+	var initial_position = Vector3(i * gap, 0, -j * gap)
+	resource.init(str(i,j),initial_position)
+	resource.connect("click",self,"_on_gui_Click")
+	add_child(resource)
+
+	for building in range(number_of_instances_adjacent_building):
+		var adjacent_position = adjacent_coordinates(initial_position, building)
+		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
+		adjacent_building.init(str(i,j,building), adjacent_position)
+		adjacent_building.connect("click",self,"_on_gui_Click")
+		add_child(adjacent_building)
 			
 func adjacent_coordinates(initial_position, building):
 	var initial_adjacent_position = Vector3(0,0,0)
@@ -55,7 +54,6 @@ func adjacent_coordinates(initial_position, building):
 	return initial_adjacent_position
  
 func _ready():
-	for type in resource_types:
-		resources[type] = []
-		input[type] = []
-		generate_sequence(type)
+	for i in range(3):
+		for j in range(3):
+			main_building_coordidate(i, j)
--- src/Scripts/ResourceUI.gd
+++ src/Scripts/ResourceUI.gd
@@ -3,18 +3,17 @@ extends Spatial
 signal click(id)
 var gui
 var _id = 0
-var _type = ""
 
 func _on_Area_input_event(_camera, event, _position, _normal, _shape_idx):
 	if (event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT ):
-		emit_signal("click",_id,_type)
+		print("click")
+		emit_signal("click",_id)
 
 func _ready():
 	var camera = get_parent().get_node("Camera")
 	gui = get_node("GUI/Render")
 	gui.rotation = camera.rotation
 
-func init(id, _initial_position,type):
+func init(id, _initial_position):
 	_id = id
-	_type = type
 	self.translation = _initial_position
