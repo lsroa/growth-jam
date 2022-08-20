6fe065fca02e7a03af88aeaf7d71f5341d690088
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-08-20 17:23:52 -0300

    feat: done refactor adjacent coordinates function

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -14,33 +14,33 @@ func _on_gui_Click(id,type):
 func generate_sequence(type):
 	for i in range(number_of_instances):
 		var resource = preload("res://Scenes/Resource.tscn").instance()
-		var initial_position = Vector3(0, 0, 0)
+		var initial_position = Vector3(randi() % 15, 0.0, randi() % 15)
 		resource.init(i,initial_position,type)
 		resource.connect("click",self,"_on_gui_Click")
 		add_child(resource)
 		resources[type].append(i)
-		for j in range(number_of_instances_adjacent_building):
-			var initial_adjacent_position = adjacent_coordinates(initial_position, j)
+		for building in range(number_of_instances_adjacent_building):
+			var adjacent_position = adjacent_coordinates(initial_position, building)
 			var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-			adjacent_building.init(i,initial_adjacent_position,type)
+			adjacent_building.init(i,adjacent_position,type)
 			adjacent_building.connect("click",self,"_on_gui_Click")
 			add_child(adjacent_building)
 			
-func adjacent_coordinates(initial_position, j):
+func adjacent_coordinates(initial_position, building):
 	var initial_adjacent_position = Vector3(0,0,0)
-	if (j == 0):
+	if (building == 0):
 		initial_adjacent_position = Vector3(
 			initial_position.x + 2, 
 			initial_position.y, 
 			initial_position.z
 		)
-	elif(j == 1):
+	elif(building == 1):
 		initial_adjacent_position = Vector3(
 			initial_position.x - 2, 
 			initial_position.y, 
 			initial_position.z 
 		)
-	elif(j == 2):
+	elif(building == 2):
 		initial_adjacent_position = Vector3(
 			initial_position.x - 0.5, 
 			initial_position.y, 
