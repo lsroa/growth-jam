7980a9ae641df6e7c74f510e333d87d42208c523
Luis Roa Castro <luis.roa@mac.local>
2022-08-20 10:18:12 +0200

    feat: Add spawner

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -1,5 +1,19 @@
 extends Spatial
+var resources = []
+var input = []
 
-func _on_SequenceUI_click(event):
-	print("click")
-	print(event)
+func _on_SequenceUI_click(id):
+	print(id)
+	input.append(id)
+
+func genereate_sequence():
+	for i in range(3):
+		var resource = preload("res://Scenes/Resource.tscn").instance()
+		var initial_position = Vector3(randi() % 7, 0.0, randi() % 7)
+		resource.init(i,initial_position)
+
+		add_child(resource)
+		resources.append(i)
+
+func _ready():
+	genereate_sequence()
--- src/Scripts/ResourceUI.gd
+++ src/Scripts/ResourceUI.gd
@@ -1,13 +1,19 @@
 extends Spatial
-signal click(event)
+
+signal click(id)
+var gui
+var id = 0
 
 func _on_Area_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton:
-		emit_signal("click",event)
+		print("click from area")
+		emit_signal("click",id)
 
 func _ready():
 	var camera = get_parent().get_node("Camera")
-	var gui = get_node("GUI")
+	gui = get_node("GUI/Render")
 	gui.rotation = camera.rotation
 
-	
+func init(_id, _initial_position):
+	id = _id
+	self.translation = _initial_position
