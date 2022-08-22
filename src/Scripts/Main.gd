70b7bd5bbf600c2e7c41e348a7368379ff2b147c
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-08-22 12:10:52 -0300

    feat: added score variable to count clicks in an adjacent building instance

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -1,21 +1,25 @@
 extends Spatial
 var resources = {} 
 var input = {}
-export(int) var number_of_instances_main_building = 9
-#export(int) var number_of_instances_adjacent_building = 3
+#TODO: 
+#export(int) var main_building_instance = 9
 export(int) var main_building_gap = 8
+
+var score = 0
 var resource_types = ["oil", "power", "water", "solar"]
 var adjancent_building_positions = ["left", "right", "up", "bottom"]
 
 func _on_gui_Click(id):
 	print("id: " , id)
+	score+=1
+	print("score: " , score)
 #	input[type].append(id)
 	print(input)
 
 func main_building_coordidate(column, row):
 	var resource = preload("res://Scenes/Resource.tscn").instance()
 	var initial_position = Vector3(column * main_building_gap, 0, - row * main_building_gap)
-	resource.init(str(column, row),initial_position)
+	resource.init("(%s,%s)" % [column, row],initial_position)
 	resource.connect("click",self,"_on_gui_Click")
 	add_child(resource)
 	assign_adjacent_buiding(column, row, initial_position)
@@ -28,7 +32,7 @@ func assign_adjacent_buiding(column, row, initial_position):
 	for adjancent_building_position in random_adjancent_building_positions:
 		var adjacent_coordinates = adjacent_coordinates(initial_position, adjancent_building_position)
 		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-		adjacent_building.init(str(column, row,adjancent_building_position), adjacent_coordinates)
+		adjacent_building.init("(%s,%s):%s" % [column, row,adjancent_building_position], adjacent_coordinates)
 		adjacent_building.connect("click",self,"_on_gui_Click")
 		add_child(adjacent_building)
 
