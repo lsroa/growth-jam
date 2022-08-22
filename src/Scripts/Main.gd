1ced9c6c6bb000684d0846f45384f5f9a0b96175
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-08-22 10:48:10 -0300

    feat: added a random behaviuor in adjacent building position

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -2,9 +2,10 @@ extends Spatial
 var resources = {} 
 var input = {}
 export(int) var number_of_instances_main_building = 9
-export(int) var number_of_instances_adjacent_building = 4
-export(int) var gap = 10
+#export(int) var number_of_instances_adjacent_building = 3
+export(int) var main_building_gap = 8
 var resource_types = ["oil", "power", "water", "solar"]
+var adjancent_building_positions = ["left", "right", "up", "down"]
 
 func _on_gui_Click(id):
 	print("id: " , id)
@@ -13,33 +14,38 @@ func _on_gui_Click(id):
 
 func main_building_coordidate(i, j):
 	var resource = preload("res://Scenes/Resource.tscn").instance()
-	var initial_position = Vector3(i * gap, 0, -j * gap)
+	var initial_position = Vector3(i * main_building_gap, 0, -j * main_building_gap)
 	resource.init(str(i,j),initial_position)
 	resource.connect("click",self,"_on_gui_Click")
 	add_child(resource)
+	
+	var random_number = rand_range(0, adjancent_building_positions.size())
+	# TODO: This random rule needs to be improved
+	var random_adjancent_building_positions = adjancent_building_positions.slice(random_number - 1, random_number)
 
-	for building in range(number_of_instances_adjacent_building):
-		var adjacent_position = adjacent_coordinates(initial_position, building)
+	for adjancent_building_position in random_adjancent_building_positions:
+		var adjacent_coordinates = adjacent_coordinates(initial_position, adjancent_building_position)
 		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-		adjacent_building.init(str(i,j,building), adjacent_position)
+		adjacent_building.init(str(i,j,adjancent_building_position), adjacent_coordinates)
 		adjacent_building.connect("click",self,"_on_gui_Click")
 		add_child(adjacent_building)
 			
-func adjacent_coordinates(initial_position, building):
+func adjacent_coordinates(initial_position, adjancent_building_position):
 	var initial_adjacent_position = Vector3(0,0,0)
-	if (building == 0):
+	
+	if (adjancent_building_position == "down"):
 		initial_adjacent_position = Vector3(
 			initial_position.x + 2, 
 			initial_position.y, 
 			initial_position.z
 		)
-	elif(building == 1):
+	elif(adjancent_building_position == "up"):
 		initial_adjacent_position = Vector3(
 			initial_position.x - 2, 
 			initial_position.y, 
 			initial_position.z 
 		)
-	elif(building == 2):
+	elif(adjancent_building_position == "right"):
 		initial_adjacent_position = Vector3(
 			initial_position.x - 0.5, 
 			initial_position.y, 
@@ -51,6 +57,7 @@ func adjacent_coordinates(initial_position, building):
 			initial_position.y, 
 			initial_position.z + 2
 		)
+
 	return initial_adjacent_position
  
 func _ready():
