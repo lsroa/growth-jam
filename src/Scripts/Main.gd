f4b5cdb9180b631864c713a720d25eae8f7ad3c4
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-08-24 09:34:09 -0300

    fix: passed logic to its own Resource script

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -21,54 +21,10 @@ func _on_gui_Click(id):
 func main_building_coordidate(column, row):
 	var resource = preload("res://Scenes/Resource.tscn").instance()
 	var initial_position = Vector3(column * main_building_gap, 0, - row * main_building_gap)
-	resource.init("(%s,%s)" % [column, row],initial_position)
+	resource.init("(%s,%s)" % [column, row], initial_position, column, row)
 	resource.connect("click",self,"_on_gui_Click")
 	add_child(resource)
-	assign_adjacent_buiding(column, row, initial_position)
-	
-func assign_adjacent_buiding(column, row, initial_position):
-	var random_number = rand_range(0, adjancent_building_positions.size())
-	# TODO: This random rule needs to be improved
-	var random_adjancent_building_positions = adjancent_building_positions.slice(random_number - 1, random_number)
 
-	for adjancent_building_position in random_adjancent_building_positions:
-		var adjacent_coordinates = adjacent_coordinates(initial_position, adjancent_building_position)
-		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-		adjacent_building.init("(%s,%s): %s" % [column, row,adjancent_building_position], adjacent_coordinates)
-		adjacent_building.connect("click",self,"_on_gui_Click")
-		add_child(adjacent_building)
-
-func adjacent_coordinates(initial_position, adjancent_building_position):
-	var initial_adjacent_position = Vector3(0,0,0)
-	
-	match (adjancent_building_position):
-		"bottom":
-			initial_adjacent_position = Vector3(
-				initial_position.x + 2, 
-				initial_position.y, 
-				initial_position.z
-			)
-		"up":
-			initial_adjacent_position = Vector3(
-				initial_position.x - 2, 
-				initial_position.y, 
-				initial_position.z 
-			)
-		"right":
-			initial_adjacent_position = Vector3(
-				initial_position.x - 0.5, 
-				initial_position.y, 
-				initial_position.z - 2
-			)
-		"left":
-			initial_adjacent_position = Vector3(
-				initial_position.x, 
-				initial_position.y, 
-				initial_position.z + 2
-			)
-
-	return initial_adjacent_position
- 
 func _ready():
 	randomize()
 	for column in range(3):
--- src/Scripts/Resource.gd
+++ src/Scripts/Resource.gd
@@ -4,6 +4,10 @@ tool
 signal click(id)
 var gui
 var _id = 0
+var _column
+var _row
+var _initial_position
+var adjancent_building_positions = ["left", "right", "up", "bottom"]
 
 
 func _ready():
@@ -11,13 +15,74 @@ func _ready():
 		var camera = get_parent().get_node("Pivot/Camera")
 		gui = get_node("GUI/Render")
 		gui.rotation = camera.rotation
+	# var camera = get_parent().get_node("Pivot/Camera")
+	# gui = get_node("GUI/Render")
+	# gui.rotation = camera.rotation
+	# print("_ready:",_id)
+
+	var random_number = rand_range(0, adjancent_building_positions.size())
+	# TODO: This random rule needs to be improved
+	var random_adjancent_building_positions = adjancent_building_positions.slice(random_number - 1, random_number)
 	
+	for adjancent_building_position in random_adjancent_building_positions:
+		var adjacent_coordinates = adjacent_coordinates(_initial_position, adjancent_building_position)
+		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
+		adjacent_building.init("(%s,%s): %s" % [_column, _row, adjancent_building_position], adjacent_coordinates)
+		adjacent_building.connect("click",self,"_on_gui_Click")
+		add_child(adjacent_building)
 
-func init(id, _initial_position):
+func assign_adjacent_buiding(column, row, initial_position):
+	var random_number = rand_range(0, adjancent_building_positions.size())
+	# TODO: This random rule needs to be improved
+	var random_adjancent_building_positions = adjancent_building_positions.slice(random_number - 1, random_number)
+
+	for adjancent_building_position in random_adjancent_building_positions:
+		var adjacent_coordinates = adjacent_coordinates(initial_position, adjancent_building_position)
+		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
+		adjacent_building.init("(%s,%s): %s" % [column, row,adjancent_building_position], adjacent_coordinates)
+		adjacent_building.connect("click",self,"_on_gui_Click")
+		add_child(adjacent_building)
+
+func adjacent_coordinates(initial_position, adjancent_building_position):
+	var initial_adjacent_position = Vector3(0,0,0)
+	
+	match (adjancent_building_position):
+		"bottom":
+			initial_adjacent_position = Vector3(
+				initial_position.x + 2, 
+				initial_position.y, 
+				initial_position.z
+			)
+		"up":
+			initial_adjacent_position = Vector3(
+				initial_position.x - 2, 
+				initial_position.y, 
+				initial_position.z 
+			)
+		"right":
+			initial_adjacent_position = Vector3(
+				initial_position.x - 0.5, 
+				initial_position.y, 
+				initial_position.z - 2
+			)
+		"left":
+			initial_adjacent_position = Vector3(
+				initial_position.x, 
+				initial_position.y, 
+				initial_position.z + 2
+			)
+
+	return initial_adjacent_position
+		 
+
+func init(id, initial_position, column, row):
 	_id = id
-	self.translation = _initial_position
+	_column = column
+	_row = row
+	_initial_position = initial_position
+	self.translation = initial_position
 
-func _on_Area_input_event(_camera, _event, _position, _normal, _shape_idx):
+func _on_Area_input_event(_camera, _event, _position, _normal, _shaperowx):
 	emit_signal("click",_id)
 
 
