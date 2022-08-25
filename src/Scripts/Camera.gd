f5afa935abe7dfd6651cc89a65a5113e2774e02a
Luis Roa <roa234@gmail.com>
2022-08-25 22:04:43 +0200

    refactor: format code with gdformat
    refactor: format

--- src/Scripts/Camera.gd
+++ src/Scripts/Camera.gd
@@ -3,17 +3,19 @@ extends Camera
 var zoom := 1.0 setget _set_zoom
 var zoom_factor = 1
 
+
 func _input(event):
 	if event.is_action("zoom_in"):
 		_set_zoom(zoom - zoom_factor)
-		
+
 	if event.is_action("zoom_out"):
 		_set_zoom(zoom + zoom_factor)
 
+
 func _set_zoom(value):
 	var tween = get_node("Tween")
-	zoom = clamp(value,15.0,30.0)
-	tween.interpolate_property(self, "size",
-			zoom, zoom, 0.2,
-			Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+	zoom = clamp(value, 15.0, 30.0)
+	tween.interpolate_property(
+		self, "size", zoom, zoom, 0.2, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT
+	)
 	tween.start()
--- src/Scripts/MeshInstancer.gd
+++ src/Scripts/MeshInstancer.gd
@@ -1,8 +1,9 @@
 extends MeshInstance
-tool 
+tool
 
 export(Array, Mesh) var mesh_pools
 export(Shader) var shader
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +12,9 @@ func _ready():
 	self.translation.y = 1
 	_assign_shader_to_material(mesh)
 
+
 func _assign_shader_to_material(mesh: Mesh):
 	for m in mesh.get_surface_count():
 		var material = mesh.surface_get_material(m)
-		material.set_shader_param("LOAD_COLOR", Color(0,1,0)) 
+		material.set_shader_param("LOAD_COLOR", Color(0, 1, 0))
 		material.shader = shader
-		
