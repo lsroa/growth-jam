38a81e06e4b9fc3254e42962b51b907dae9cec3f
José Cabrera <40355414+Jose-Cabrera-FullStack@users.noreply.github.com>
2022-08-25 17:08:33 -0300

    Sequence player (#1)
    feat: Add UI for debugging coordinates

--- src/Scripts/Adjacent.gd
+++ src/Scripts/Adjacent.gd
@@ -8,31 +8,33 @@ var fade_material
 var building
 
 func _ready():
-#	gui.rotation = camera.rotation
 	building = get_node("Building")
 	fade_material = get_node("Fade")
+	label_position_gui()
+
+func label_position_gui():
+	var label = get_node("Spatial/Viewport/Label")
+	label.text = "(%s, %s, %s)" % [self.translation.x, self.translation.y, self.translation.z]
 
 func init(id, _initial_position):
 	_id = id
 	self.translation = _initial_position
 
 func _on_StaticBody_input_event(_camera, event, _position, _normal, _shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_MASK_LEFT:
-			flash()
-			emit_signal("click",_id)
-			
+	if event is InputEventMouseButton and event.button_index == BUTTON_MASK_LEFT and event.pressed:
+		flash()
+		emit_signal("click",_id)
+
 func flash():
 	var material = building.get_active_material(0)
 	fade_material.interpolate_property(
-		material, 
-		"shader_param/level", 
-		1.0, 
-		0.0, 
+		material,
+		"shader_param/level",
+		1.0,
+		0.0,
 		0.5,
-		Tween.TRANS_LINEAR, 
+		Tween.TRANS_LINEAR,
 		Tween.EASE_IN
 	)
 	fade_material.start()
-	
-	
+
--- src/Scripts/Resource.gd
+++ src/Scripts/Resource.gd
@@ -1,82 +1,90 @@
 extends Spatial
 tool
 
-signal click(id)
 var gui
 var _id = 0
-var _column
-var _row
-var _initial_position
+
+export (PackedScene) var adjacent_building
 var adjancent_building_positions = ["left", "right", "up", "bottom"]
+
+enum sequence_status {active, finish}
+var current_sequence = ["left","left","left","right"]
+var player_sequence = "left"
+
+#TODO: pass this variable to a global scope
 var score = 0
 
 
+func _on_gui_Click(adjancent_building_position):
+#	score+=1
+	var current_value_sequence = current_sequence.pop_front()
+	if current_value_sequence == adjancent_building_position:
+		print("player_sequence: " , current_value_sequence)
+		print("player_sequence: " , current_sequence)
 
-func _on_gui_Click(id):
-	print("id: " , id)
-	score+=1
-	print("score: " , score)
-#	input[type].append(id)
+func validate_player_sequence():
+	pass
 
 func _ready():
 	if not Engine.editor_hint:
 		var camera = get_parent().get_node("Pivot/Camera")
 		gui = get_node("GUI/Render")
 		gui.rotation = camera.rotation
-	assign_adjacent_buiding(_column, _row, _initial_position)
+		label_position_gui()
 
+func label_position_gui():
+	var label = get_node("Spatial/Viewport/Label")
+	label.text = "(%s, %s, %s)" % [self.translation.x, self.translation.y, self.translation.z]
+	assign_adjacent_buiding()
 
-func assign_adjacent_buiding(column, row, initial_position):
+func assign_adjacent_buiding():
 	var random_number = rand_range(0, adjancent_building_positions.size())
 	# TODO: This random rule needs to be improved
 	var random_adjancent_building_positions = adjancent_building_positions.slice(random_number - 1, random_number)
 
 	for adjancent_building_position in random_adjancent_building_positions:
-		var adjacent_coordinates = adjacent_coordinates(initial_position, adjancent_building_position)
-		var adjacent_building = preload("res://Scenes/AdjacentBuilding.tscn").instance()
-		adjacent_building.init("(%s,%s): %s" % [column, row,adjancent_building_position], adjacent_coordinates)
-		adjacent_building.connect("click",self,"_on_gui_Click")
-		add_child(adjacent_building)
+		var adjacent_coordinates = adjacent_coordinates(adjancent_building_position)
+		var new_adjacent_building = adjacent_building.instance()
 
-func adjacent_coordinates(initial_position, adjancent_building_position):
-	var initial_adjacent_position = Vector3(0,0,0)
+		new_adjacent_building.init("%s" % [adjancent_building_position], adjacent_coordinates)
+		new_adjacent_building.connect("click", self, "_on_gui_Click")
+		add_child(new_adjacent_building)
+
+func adjacent_coordinates(adjancent_building_position):
 
 	match (adjancent_building_position):
 		"bottom":
-			initial_adjacent_position = Vector3(
-				initial_position.x + 2,
-				initial_position.y,
-				initial_position.z
+			return Vector3(
+				self.translation.x + 2,
+				self.translation.y,
+				self.translation.z
 			)
 		"up":
-			initial_adjacent_position = Vector3(
-				initial_position.x - 2,
-				initial_position.y,
-				initial_position.z
+			return Vector3(
+				self.translation.x - 2,
+				self.translation.y,
+				self.translation.z
 			)
 		"right":
-			initial_adjacent_position = Vector3(
-				initial_position.x - 0.5,
-				initial_position.y,
-				initial_position.z - 2
+			return Vector3(
+				self.translation.x - 0.5,
+				self.translation.y,
+				self.translation.z - 2
 			)
 		"left":
-			initial_adjacent_position = Vector3(
-				initial_position.x,
-				initial_position.y,
-				initial_position.z + 2
+			return Vector3(
+				self.translation.x,
+				self.translation.y,
+				self.translation.z + 2
 			)
 
-	return initial_adjacent_position
 
-func init(id, initial_position, column, row):
+func init(id, initial_position):
 	_id = id
-	_column = column
-	_row = row
-	_initial_position = initial_position
 	self.translation = initial_position
 
 func _on_Area_input_event(_camera, _event, _position, _normal, _shaperowx):
-	emit_signal("click",_id)
+	pass
+	# emit_signal("click",_id)
 
 
