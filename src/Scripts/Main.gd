cc53e43a24209d3e0dd71fa164159c1d22c20175
Luis Roa <roa234@gmail.com>
2022-08-28 14:24:27 +0200

    fix: Adjecent initial coordinates

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -3,8 +3,7 @@ tool
 
 var resources = {}
 var input = {}
-#TODO:
-#export(int) var main_building_instance = 9
+
 export(int) var main_building_gap = 8
 
 var resource_types = ["oil", "power", "water", "solar"]
@@ -13,8 +12,9 @@ var resource_types = ["oil", "power", "water", "solar"]
 func main_building_coordidate(column, row):
 	var resource = preload("res://Scenes/Resource.tscn").instance()
 	var initial_position = Vector3(column * main_building_gap, 0, -row * main_building_gap)
-	resource.init("(%s,%s)" % [column, row], initial_position)
 	add_child(resource)
+	resource.init("(%s,%s)" % [column, row], initial_position)
+	
 
 
 func _ready():
