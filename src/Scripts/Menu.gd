637ad371972921b6eb8305cf58077b85a5a69ec4
Luis Francisco Roa <roa234@gmail.com>
2022-09-01 14:09:18 +0200

    feat: add basic menu

---
+++ src/Scripts/Menu.gd
@@ -0,0 +1,23 @@
+extends Spatial
+export onready var level = -0.5
+onready var material = get_node("Area/MeshInstance").get_active_material(0)
+var is_on_hold = false
+onready var pivot = get_parent().get_node("Pivot")
+onready var only_once = true
+
+
+func _process(delta):
+	if(is_on_hold):
+		level = level + 0.05
+		material.set_shader_param("level", level)
+	if(level >= 1.0 and only_once):
+		pivot.move_to_start()
+		only_once = false
+
+func remove():
+	get_parent().remove_child(self)
+
+func _on_Button_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_MASK_LEFT:
+		is_on_hold = event.pressed
+	
