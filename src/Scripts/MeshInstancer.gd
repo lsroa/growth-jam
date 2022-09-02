cdb71d49100fc4b5b50d2f93a400ab7e84a10796
Luis Francisco Roa <roa234@gmail.com>
2022-09-02 11:52:47 +0200

    reafactor: Dynamic material of main building

--- src/Scripts/MeshInstancer.gd
+++ src/Scripts/MeshInstancer.gd
@@ -5,20 +5,16 @@ export(Array, Mesh) var mesh_pools
 export(Shader) var shader
 onready var load_material = get_node("Tween")
 export(Array, Color) var colors
+onready var material = ShaderMaterial.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.mesh = mesh_pools[randi() % 4].duplicate()
 	self.mesh.surface_set_material(0,material)
 	self.translation.y = 1
-	_assign_shader_to_material(mesh)
+	material.set_shader_param("LOAD_COLOR", colors[randi() % colors.size()])
+	material.shader = shader
 
-
-func _assign_shader_to_material(mesh: Mesh):
-	for m in mesh.get_surface_count():
-		var material = mesh.surface_get_material(m)
-		material.set_shader_param("LOAD_COLOR", colors[randi() % colors.size()])
-		material.shader = shader
 
 func init(cooldown):
 	load_material.interpolate_property(
