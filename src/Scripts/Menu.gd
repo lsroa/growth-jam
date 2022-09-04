a7855ed3a7aa45dfc3155e50e14f4b3d1df34062
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-09-04 10:48:39 -0300

    feat: added background song

--- src/Scripts/Menu.gd
+++ src/Scripts/Menu.gd
@@ -1,8 +1,9 @@
 extends Spatial
 export onready var level = -0.5
-onready var material = get_node("Area/MeshInstance").get_active_material(0)
 var is_on_hold = false
 onready var pivot = get_parent().get_node("Pivot")
+onready var material = get_node("Area/MeshInstance").get_active_material(0)
+onready var music = get_parent().get_node("BackgroundMusic")
 onready var only_once = true
 
 
@@ -18,4 +19,5 @@ func _process(_delta):
 func _on_Button_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_MASK_LEFT:
 		is_on_hold = event.pressed
-	
+		music.volume_db = -20
+		music.play()
